cd6132ff460837aa4bdb347a2b0eb0a6caf2bf4b
Tolleiv Nietsch <info@tolleiv.de>
2011-05-19 13:48:18 +0200

    Removed the displayCond for the tx_realurl_pathsegment field - it's still used so we can't hide it

--- ext_tables.php
+++ ext_tables.php
@@ -18,7 +18,7 @@ if (TYPO3_MODE=='BE')	{
 $TCA['pages']['columns'] += array(
 	'tx_realurl_pathsegment' => array(
 		'label' => 'LLL:EXT:realurl/locallang_db.xml:pages.tx_realurl_pathsegment',
-		'displayCond' => 'FIELD:tx_realurl_exclude:!=:1',
+//		'displayCond' => 'FIELD:tx_realurl_exclude:!=:1',
 		'exclude' => 1,
 		'config' => array (
 			'type' => 'input',
